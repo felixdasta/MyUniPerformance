70c31d87d1c465c6b726717a30ecf60c9b22ece3
Arai <araiguma.programming@gmail.com>
2022-03-03 16:19:59 -0400

    [FEAT] dummy commit

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from api.models.student import Student
+from api.models.course import Course
+
 
 class StudentSerializer(ModelSerializer):
     class Meta:
         model = Student
         fields = ['user_id', 'first_name', 'last_name', 'year_of_admission', 'institutional_email', 'password', 'curriculums']
+
+class CourseSerializer(ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['course_id', 'course_name', 'course_code', 'course_credits', 'department_id']
