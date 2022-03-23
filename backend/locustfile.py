38ac4b18f154189fa7e23f2bd4716e111d046238
armando0598 <armando.leon@upr.edu>
2022-03-22 21:14:06 -0400

    Tracking locust file

---
+++ backend/locustfile.py
@@ -0,0 +1,19 @@
+#from locust import HttpUser, task, between
+
+#class LoadTesting(HttpUser):
+#    wait_time = between(0.5, 2.5)
+
+
+#    @task
+#    def get_sections(self):
+#        self.client.get('/sections')
+
+#    @task
+#    def get_curriculums(self):
+#        self.client.get('/curriculums')
+
+#    @task
+#    def get_students(self):
+#        self.client.get('/students')
+
+        
