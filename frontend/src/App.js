6de68319e8704a93693b801253ac43bc73688fc3
Arai <araiguma.programming@gmail.com>
2022-04-24 13:18:43 -0400

    Refactored instructors pages, now has details view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Home from "./containers/Home";
 import Login from "./containers/Login/Login";
 import SignUp from "./containers/Sign Up/SignUp";
 import Instructors from "./containers/Instructors/Instructors";
+import InstructorDetails from "./containers/InstructorDetails/InstructorDetails";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/instructors" element={<Instructors />} />
+        <Route path="/instructors/details" element={<InstructorDetails />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/courses/details" element={<CourseDetails />} />
         <Route path="/curriculum" element={<Curriculum />} />
--- frontend/src/containers/Instructors/Instructors.js
+++ frontend/src/containers/Instructors/Instructors.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Button } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import * as Loader from "react-loader-spinner";
@@ -21,29 +21,26 @@ export default function Instructors() {
         })
     }, [])
 
+    const viewInstructorDetails = () => {
+        navigate("details")
+    }
+
     return (
         <Box sx={{ mx: 3, my: 3 }}> {student ? <Grid container spacing={0} columnSpacing={3} rowGap={3}>
             {/* Top Row Container, each item container can be adjusted for width by changing lg*/}
-            <Grid item container lg={12} justifyContent="center" columnSpacing={3}>
-                <Grid item container lg={4} justifyContent="center">
-                    <Grid item component={Box} lg={12} sx={{ backgroundColor: "salmon", height: "250px" }}> Instructor info </Grid>
-                </Grid>
-                <Grid item container lg={4} justifyContent="center">
-                    <Grid item component={Box} lg={12} sx={{ backgroundColor: "powderblue", height: "200px" }}> Image and feedback button </Grid>
-                </Grid>
-                <Grid item container lg={4} justifyContent="center">
-                    <Grid item component={Box} lg={12} sx={{ backgroundColor: "lime", height: "400px" }}> Quick facts </Grid>
-                </Grid>
+            <Grid item container lg={3} justifyContent="center">
+                <Grid item component={Box} lg={12} sx={{ backgroundColor: "powderblue", height: "700px" }}> Search Filters </Grid>
             </Grid>
-
-            {/* Bottom Row Container */}
-            <Grid item container lg={12} justifyContent="center">
-                <Grid item component={Box} lg={7} sx={{ backgroundColor: "blueviolet", height: "350px" }}> Course list </Grid>
-                <Grid item component={Box} lg={5} sx={{ backgroundColor: "navajowhite", height: "350px" }}> Course info </Grid>
+            <Grid item container lg={9} justifyContent="center">
+                <Grid item component={Box} lg={12} sx={{ backgroundColor: "crimson", height: "900px" }}> Instructor List </Grid>
+            </Grid>
+            <Grid item container lg={2} justifyContent="center">
+                <Grid item component={Button} lg={12} onClick={viewInstructorDetails}> Details </Grid>
             </Grid>
         </Grid> : <div className="loader">
             <Loader.ThreeDots color="black" height={120} width={120} />
 
-        </div>}</Box>
+        </div>
+        }</Box >
     );
 }
