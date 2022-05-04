1e39a5141977c4b5a824f98a8f1d323941624d0a
Three Partners Softworks <felix.dasta@upr.edu>
2022-05-04 02:14:42 -0400

    Debug mode set to false (deployed backend purpose)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ REAL_BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'myuniperformance-backend.herokuapp.com']
 
