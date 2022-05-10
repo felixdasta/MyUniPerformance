ca0c7e6d1f9399bc8e7561460c8faf4ce6a696a0
Arai <araiguma.programming@gmail.com>
2022-05-10 13:35:50 -0400

    [CKPT] list now accurately shows missing courses

--- frontend/src/components/CurriculumCoursePicker/CurriculumCoursePicker.js
+++ frontend/src/components/CurriculumCoursePicker/CurriculumCoursePicker.js
@@ -1,31 +1,84 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import InfiniteScroll from 'react-infinite-scroller';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
-import { Divider, IconButton, List } from "@mui/material";
+import { FormControl, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, IconButton, InputLabel, List, MenuItem, Select, TextField } from "@mui/material";
+import { get_sections_grades_stats } from "../../actions/sections";
 
 export default function CurriculumCoursePicker(props) {
-    let [coursesCurriculum, setCoursesCurriculum] = useState([]);
-    let [coursesTaken, setCoursesTaken] = useState();
-    let [filters, setFilters] = useState();
+    const [filters, setFilters] = useState();
+    const [open, setOpen] = useState(false);
+    const [formData, setFormData] = useState({ year: "", semester: "", section_code: "", grade: "" });
+    const [course, setCourse] = useState();
+    const [missingCourses, setMissingCourses] = useState([]);
+
+    const grade_list = ["IP", "A", "B", "C", "D", "F", "P", "W", "IB", "ID", "IC", "IF"]
+
+    const handleClickOpen = useCallback((course) => {
+        setOpen(true);
+        setCourse(course);
+    }, []);
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleSubmit = () => {
+        setOpen(false);
+    };
+
+    const handleFormData = (e) => {
+        console.log(course)
+        switch (e.target.name) {
+            case "year":
+                setFormData({ ...formData, year: e.target.value })
+                break;
+            case "semester":
+                setFormData({ ...formData, semester: e.target.value })
+                break;
+            case "section_code":
+                setFormData({ ...formData, section_code: e.target.value })
+                break;
+            case "grade":
+                setFormData({ ...formData, grade: e.target.value })
+                break;
+            default:
+                break;
+        }
+    }
 
     useEffect(() => {
-        setCoursesCurriculum(props.student.curriculums[0].courses)
+        let curriculumCourses = props.student.curriculums[0].courses
+        let coursesTaken = []
+        let coursesMissing = []
 
-    }, [])
+        // filter for missing courses
+        props.student.enrolled_sections.forEach(record => {
+            coursesTaken.push(record.section.course.course_code)
+        });
 
-    if (coursesCurriculum !== undefined) {
+        // extract missing courses
+        curriculumCourses.forEach(record => {
+            if (!coursesTaken.includes(record.course.course_code)) {
+                coursesMissing.push(record)
+            }
+        });
+
+        setMissingCourses(coursesMissing)
+    }, [open])
+
+    if (missingCourses) {
         return (
-            <Box boxShadow={1} sx={{ border: '1px' }}>
+            <Box boxShadow={1} height={716} sx={{ border: '1px' }}>
                 <List sx={{ width: '100%', height: 700, maxWidth: 360, bgcolor: 'background.paper', overflow: 'auto' }}>
-                    <InfiniteScroll>
-                        {coursesCurriculum.map((course) => (
+                    {<InfiniteScroll>
+                        {missingCourses.map((course) => (
                             <div>
                                 <ListItem
                                     secondaryAction={
-                                        <IconButton edge="end" aria-label="submit">
+                                        <IconButton edge="end" aria-label="submit" onClick={() => {
+                                            handleClickOpen(course)
+                                        }}>
                                             <DoubleArrowIcon />
                                         </IconButton>
                                     }
@@ -38,8 +91,84 @@ export default function CurriculumCoursePicker(props) {
                                 </ListItem>
                                 <Divider variant="middle" />
                             </div>))}
-                    </InfiniteScroll>
+                    </InfiniteScroll>}
                 </List>
+                <Dialog open={open} onClose={handleClose} maxWidth={"md"}>
+                    <DialogTitle>Add {course ? course.course.course_code : "None"} to Curriculum</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please choose a valid year and/or section code as well as the grade obtained for this course
+                        </DialogContentText>
+
+                        {/* Year input */}
+                        <FormControl variant="standard" sx={{ m: 1.5, minWidth: 120 }}>
+                            <InputLabel id="year-label">Year</InputLabel>
+                            <Select
+                                value={formData.year}
+                                onChange={handleFormData}
+                                label="year"
+                                name="year"
+                            >
+                                <MenuItem value="">
+                                    <em>-</em>
+                                </MenuItem>
+                                <MenuItem value={1990}>1990</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                        {/* Semester input */}
+                        <FormControl variant="standard" sx={{ m: 1.5, minWidth: 120 }}>
+                            <InputLabel id="semester-label">Semester</InputLabel>
+                            <Select
+                                value={formData.semester}
+                                onChange={handleFormData}
+                                label="semester"
+                                name="semester"
+                            >
+                                <MenuItem value="">
+                                    <em>-</em>
+                                </MenuItem>
+                                <MenuItem value={1990}>1990</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                        {/* Section Code input */}
+                        <FormControl variant="standard" sx={{ m: 1.5, minWidth: 120 }}>
+                            <InputLabel id="section_code-label">Section Code</InputLabel>
+                            <Select
+                                value={formData.section_code}
+                                onChange={handleFormData}
+                                label="section_code"
+                                name="section_code"
+                            >
+                                <MenuItem value="">
+                                    <em>-</em>
+                                </MenuItem>
+                                <MenuItem value="MATE3032">MATE3032</MenuItem>
+                            </Select>
+                        </FormControl>
+
+                        {/* Grade input */}
+                        <FormControl variant="standard" sx={{ m: 1.5, minWidth: 120 }}>
+                            <InputLabel id="grade-label">Grade</InputLabel>
+                            <Select
+                                value={formData.grade}
+                                onChange={handleFormData}
+                                label="grade"
+                                name="grade"
+                            >{grade_list.map((grade) => (
+                                (grade == "IP") ? <MenuItem value={grade}>In Progress</MenuItem> : <MenuItem value={grade}>{grade}</MenuItem>
+                            ))}
+                            </Select>
+                        </FormControl>
+
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color={"error"}>Cancel</Button>
+                        <Button onClick={handleSubmit} variant={"contained"}>Submit</Button>
+                    </DialogActions>
+
+                </Dialog>
             </Box >
 
         );
